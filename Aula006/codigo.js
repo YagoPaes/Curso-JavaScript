be56a5b7f3c159bd3c98a49e64c6a0283f6ecd16
YagoPaes <yagopaes2@gmail.com>
2023-11-27 18:41:27 -0300

    update

---
+++ Aula006/codigo.js
@@ -0,0 +1,4 @@
+
+var nome = window.prompt('qual seu nome?')
+
+window.alert('É um prazer conhece-lo, ' + nome + '!') //o simbolo de + Significa concatenação (juntar)
---
+++ Aula006/codigo2.js
@@ -0,0 +1,6 @@
+var n1 = Number(window.prompt('Digite um numero: ')) // o prompt converte qualquer valor para string
+var n2 = Number(window.prompt('Digite outro numero: '))// é necessario colocar o Number() para converter
+var s = n1 + n2
+window.alert(`A soma dos valores é: ${s}`)
+//(number + number) = adição
+//(string + string) = concatenação
