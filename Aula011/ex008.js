cc5fb209c892e42511fc1de1f20873fca4d9264b
YagoPaes <yagopaes2@gmail.com>
2023-12-02 15:27:53 -0300

    update

---
+++ Aula011/ex008.js
@@ -0,0 +1,9 @@
+var vel = 12
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade multado`)
+}
+
+console.log(`Dirija com segurança`)
