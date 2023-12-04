889430912c115ca894a07d584193a39ef3a2a45d
YagoPaes <yagopaes2@gmail.com>
2023-12-03 21:10:01 -0300

    update

---
+++ Aula012/ex012.js
@@ -0,0 +1,10 @@
+var idade = 18
+
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade >= 16 && idade < 18) {
+        console.log('Voto Opcional')
+
+} else {
+    console.log('Voto Obrigatório')
+}
---
+++ Aula012/ex013.js
@@ -0,0 +1,11 @@
+
+var hora = 5
+    console.log(`Agora são extamente ${hora} horas.`)
+if (hora >= 6) {
+    console.log('Bom Dia')
+} else if (hora > 12 && hora <= 18) {
+    console.log('Boa Tarde')
+} else if (hora > 18) {
+    console.log('Boa Noite')
+} else (hora <= 6) 
+    console.log('Boa Madrugada')
