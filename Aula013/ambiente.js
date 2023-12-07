af0e813b0ce6ce8cab527b123d26724b1805eb1f
YagoPaes <yagopaes2@gmail.com>
2023-12-07 18:42:07 -0300

    update

---
+++ Aula013/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // é igual c = c + 1
+
+} while (c < 6)
+
+
+/* só a repetição while
+var c = 1
+while (c < 10) {
+    console.log(`Passo ${c}`)
+    c++ // é igual c = c + 1
+
+}
+*/
+
+
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
---
+++ Aula014/ambiente.js
@@ -0,0 +1,12 @@
+/*var c = 1
+while (c < 10) {
+    console.log(`Passo ${c}`)
+    c++ // é igual c = c + 1
+
+}*/ 
+
+console.log('Vai começar!')
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+console.log('FIM!')
